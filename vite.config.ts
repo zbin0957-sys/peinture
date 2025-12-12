513366db2ac49347e101e74bd8985964d7e8ef8d
zbin0957-sys <zbin0957@gmail.com>
2025-12-12 15:25:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'dist';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
